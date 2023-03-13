ea52bdbb6aff0d5de15a8ba9d2fc4139cc4abab4
Ruslan <ruslan.effect@gmail.com>
2023-03-13 20:32:24 +0600

    "first step"

--- src/main/java/com/mainTeam/Hakaton/entity/User.java
+++ src/main/java/com/mainTeam/Hakaton/entity/User.java
@@ -1,4 +1,26 @@
 package com.mainTeam.Hakaton.entity;
 
+import com.mainTeam.Hakaton.enums.Role;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+import static javax.persistence.EnumType.STRING;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "users")
 public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String login;
+    private String password;
+    private String phoneNumber;
+    @Enumerated(value = STRING)
+    private Role role;
 }
