7aada6c28a48116ad6047f6273c9e20feb53d4d5
askat122333 <110248679+askat1223@users.noreply.github.com>
2023-03-16 19:20:21 +0600

    добавить коллекции

---
+++ src/main/java/com/mainTeam/Hakaton/controller/AdminController.java
@@ -0,0 +1,39 @@
+package com.mainTeam.Hakaton.controller;
+
+import com.mainTeam.Hakaton.entity.Pnr;
+import com.mainTeam.Hakaton.model.UserDto;
+import com.mainTeam.Hakaton.service.PnrService;
+import com.mainTeam.Hakaton.service.UserService;
+import lombok.AllArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@AllArgsConstructor
+@RequestMapping("/Admin")
+public class AdminController {
+    @Autowired
+    PnrService pnrService;
+    @Autowired
+    UserService userService;
+    @GetMapping ("/PnrReader/{pnr}")
+    public String getPnrInNorm(@PathVariable String pnr){
+        return pnrService.getPnrInNorm(pnr);
+    }
+    @PutMapping("/Pnr/update")
+    public Pnr updatePnr(Pnr pnr) {
+        return pnrService.updatePnr(pnr);
+    }
+    @GetMapping("/User/{id}")
+    public UserDto getById (@PathVariable Long id) {
+        return userService.getById(id);
+    }
+    @GetMapping("/User/all")
+    public List<UserDto> getAll () {
+        return userService.getAllUser();
+    }
+/*    @DeleteMapping("/Pnr")
+    public String delete*/
+}
--- src/main/java/com/mainTeam/Hakaton/controller/UserController.java
+++ src/main/java/com/mainTeam/Hakaton/controller/UserController.java
@@ -1,10 +1,19 @@
 package com.mainTeam.Hakaton.controller;
 
+import com.mainTeam.Hakaton.service.PnrService;
 import lombok.AllArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @AllArgsConstructor
 public class UserController {
-
+    @Autowired
+    PnrService pnrService;
+    @GetMapping("/PnrReader/{pnr}")
+    public String getPnrInNorm(@PathVariable String pnr){
+        return pnrService.getPnrInNorm(pnr);
+    }
 }
